25e93b5ebf25e10b1bbb81313fa59506159bd52c
choibohui <123dfga@naver.com>
2023-04-05 15:40:25 +0900

    upload default template

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function () {
+
+    //스크롤 시 메뉴 변경
+
+    // $(window).on('scroll', function () {
+    //     var sct = $(window).scrollTop();
+    //     if (sct > 500) {
+    //         $('.toTop').fadeIn(300)
+    //     } else {
+    //         $('.toTop').fadeOut(1000)
+    //     }
+    // });
+
+
+
+
+    //스크롤 시 메뉴 변경
+
+
+
+
+
+
+
+
+    // main_visual 슬라이더 Start
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        // useTransform: false,
+    });
+
+    $('.main_slider').on('afterChange', function (e, s, c) {
+        console.log(e, s, c);
+        $('.slide_util .slide_pager .current_index').text("0" + (c ? c + 1 : 1))
+        $('.slide_util .slide_pager .bar_progress').addClass('on')
+    });
+
+    $('.main_slider').on('beforeChange', function () {
+        $('.slide_util .slide_pager .bar_progress').removeClass('on')
+    });
+
+    // best 슬라이더 Start
+    $('.product_slider').slick({
+        arrows: false,
+        autoplay: false,
+        // useTransform: false,
+        slidesToShow: 4,
+    });
+
+    // 슬라이더 버튼
+    $('.product_slide_arrows .hover_area_left .prev').on('click', function () {
+        $('.product_slider').slick('slickPrev')
+    });
+    $('.product_slide_arrows .hover_area_right .next').on('click', function () {
+        $('.product_slider').slick('slickNext')
+    });
+})
