42c68e9f2f560ccac4a5b542bf2d33fa714926af
choibohui <123dfga@naver.com>
2023-04-17 17:07:35 +0900

    gpt야 고마워

--- js/main.js
+++ js/main.js
@@ -28,40 +28,44 @@ $(function () {
     });
 
     // 슬라이더 버튼
-    // $('.product_slide_arrows .hover_area_left .prev').on('click', function () {
-    //     $('.product_slider').slick('slickPrev')
-    // });
-    // $('.product_slide_arrows .hover_area_right .next').on('click', function () {
-    //     $('.product_slider').slick('slickNext')
-    // });
+    $('.topbanner .area_left').on('click', function () {
+        $('.banner_slider').slick('slickPrev')
+    });
+    $('.topbanner .area_right').on('click', function () {
+        $('.banner_slider').slick('slickNext')
+    });
     // topbanner 슬라이더 End
 
 
     // 메뉴 호버 Start
-    let main_menu = $('.header .main_menu');
-    // let backdrop = $('.backdrop');
-    let nav = $('.nav');
-
-    $(main_menu).on({
-        "mouseover": function () {
-            // console.log("Mouse Over");
-            nav.addClass('active')
-        },
-        "mouseout": function () {
-            // console.log("Mouse Out");
-            nav.removeClass('active')
-        }
-    });
-    // 나중에 에러 뜰수도 있는데, 작동하고나면 멈춰! 한번 해줘야함
-
-    // main_menu 안에 submenu가 존제할 때 nav에 클레스 active를 붙여라! 아니면 안붙여도 된다. 로 풀면 좋을 것 같음.
+    // let main_menu = $('.header .main_menu');
+    // let nav = $('.nav');
 
     // $(main_menu).on({
     //     "mouseover": function () {
     //         // console.log("Mouse Over");
-    //         // nav.addClass('active')
-    //         if (main_menu.hasClass("submenu")) {
+    //         nav.addClass('active')
+    //     },
+    //     "mouseout": function () {
+    //         // console.log("Mouse Out");
+    //         nav.removeClass('active')
+    //     }
+    // });
+    // 나중에 에러 뜰수도 있는데, 작동하고나면 멈춰! 한번 해줘야함
+
+    // main_menu 안에 submenu가 존제할 때 nav에 클레스 active를 붙여라! 아니면 안붙여도 된다. 로 풀면 좋을 것 같음.
+
+    // let main_menu = $('.header .main_menu');
+    // let nav = $('.nav');
+    // let has_submenu = $('.header .main_menu>li')
+
+    // $(main_menu).on({
+    //     "mouseover": function () {
+    //         if (has_submenu.find('submenu')) {
     //             nav.addClass('active')
+    //         }
+    //         else {
+    //             nav.removeClass('active')
     //         }
     //     },
     //     "mouseout": function () {
@@ -69,6 +73,25 @@ $(function () {
     //         nav.removeClass('active')
     //     }
     // });
+
+    // 해당 메뉴 hover시 backdrop필터가 깔리는 부분은 위와같은 다양한 시도에도 불구하고 끝내 답을 찾지 못하여 chat GPT를 이용해서 코드를 작성하였음을 알립니다.
+    $(".menu").on({
+        "mouseover": function () {
+            if ($(this).has(".submenu").length > 0) {
+                $(".nav").addClass("active");
+                console.log("여기 서브메뉴 있음")
+            }
+        },
+        "mouseout": function () {
+            $(".nav").removeClass("active");
+            console.log("마우스 아웃됐음")
+        }
+    });
+
+
+
+
+
 
 
     // main_visual 슬라이더 Start
