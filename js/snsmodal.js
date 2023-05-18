a645cd446af314bccfe3c4888d5a12fd90f06b57
choibohui <123dfga@naver.com>
2023-05-18 12:31:41 +0900

    modal update

--- js/snsmodal.js
+++ js/snsmodal.js
@@ -1,3 +1,24 @@
 $(function () {
 
-})
+    // sns 슬라이더 Start
+    $('.modal .snsArea .snsSlider').slick({
+        arrows: false,
+        autoplay: false,
+        // useTransform: false,
+        dots: true,
+        dotsClass: 'sns-modal-custom-dots',
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // variableWidth: true,
+    });
+
+    // 제품 좋아요 버튼
+    $('.modal .productArea .cmlink_btn .addWishlist').on('click', function () {
+        $(this).toggleClass('plusWish');
+    });
+
+
+
+
+});
