7300df0f8f0ea2c84096ecb0e890917fd99abb1f
choibohui <123dfga@naver.com>
2023-06-15 19:27:50 +0900

    map update

---
+++ js/kakaomap.js
@@ -0,0 +1,26 @@
+// 제이쿼리로 작업중임.. 자스랑 섞어 쓰면 에러남..!
+$(function () {
+    function offlinestore() {
+
+        // $('.switch').on('click', function () {
+        //     $(this).toggleClass("stop");
+        // })
+    };
+
+    // 함수 호출
+    offlinestore();
+
+
+    // 리사이즈 됐을 때
+    $(window).on('resize', function () {
+
+
+    });
+
+
+
+
+
+
+
+});
