9ab59748503bbce4dccde6dab4e3a1eb9c84eb65
choibohui <123dfga@naver.com>
2023-06-16 17:31:39 +0900

    map update

--- js/kakaomap.js
+++ js/kakaomap.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var imageSrc = 'https://choibohui.github.io/hera/img/icon/map_marker.svg'; // 마커이미지의 주소입니다    
     var imageSize = new kakao.maps.Size(40, 50); // 마커이미지의 크기입니다
-    var imageOption = { offset: new kakao.maps.Point(20, 25) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    var imageOption = { offset: new kakao.maps.Point(20, 50) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
