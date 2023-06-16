f2849a4eb6ad01ac5f500eeb8215c81ae8a2bcfd
choibohui <123dfga@naver.com>
2023-06-16 17:16:41 +0900

    map update

--- js/kakaomap.js
+++ js/kakaomap.js
@@ -13,6 +13,14 @@ document.addEventListener('DOMContentLoaded', function () {
     for (var i = 0; i < anchors.length; i++) {
         anchors[i].addEventListener('click', function (event) {
             event.preventDefault(); // 기본 링크 동작 방지
+            // $(this).parent().addClass("select").siblings().removeClass("active");
+            this.parentNode.classList.add("select");
+            var siblings = Array.from(this.parentNode.parentNode.children);
+            siblings.forEach(function (element) {
+                if (element !== this.parentNode) {
+                    element.classList.remove("active");
+                }
+            });
 
             var lat = parseFloat(this.getAttribute('data-lat'));
             var lng = parseFloat(this.getAttribute('data-lng'));
@@ -28,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var imageSrc = 'https://choibohui.github.io/hera/img/icon/map_marker.svg'; // 마커이미지의 주소입니다    
     var imageSize = new kakao.maps.Size(40, 50); // 마커이미지의 크기입니다
-    var imageOption = { offset: new kakao.maps.Point(0, 0) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    var imageOption = { offset: new kakao.maps.Point(21, 25) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
