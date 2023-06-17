c30e50c56563a2c5cecacf93f3044c09efcaaebb
choibohui <123dfga@naver.com>
2023-06-17 18:07:50 +0900

    map update

--- js/kakaomap.js
+++ js/kakaomap.js
@@ -68,7 +68,7 @@
 $(function () {
     let mapContainer = $('#map'); // 지도를 표시할 div 
     let mapOption = {
-        center: new kakao.maps.LatLng(37.52611, 126.92862), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(37.52611, 126.92862), // 지도의 초기 중심좌표
         level: 4 // 지도의 확대 레벨
     };
 
@@ -87,6 +87,30 @@ $(function () {
             // 마커의 위치도 변경
             marker.setPosition(newCenter);
         });
+
+        let imageSrc = 'https://choibohui.github.io/hera/img/icon/map_marker.svg'; // 마커이미지의 주소입니다    
+        let imageSize = new kakao.maps.Size(40, 50); // 마커이미지의 크기입니다
+        let imageOption = { offset: new kakao.maps.Point(22, 50) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+        let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+        let markerPosition = new kakao.maps.LatLng(37.52611, 126.92862); // 초기 마커의 표시될 위치입니다
+
+        // 마커를 생성합니다
+        let marker = new kakao.maps.Marker({
+            position: markerPosition,
+            image: markerImage // 마커이미지 설정 
+        });
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+
+        // 매장 리스트 선택 활성화
+        $('.storeList li').click(function () {
+            $(this).addClass('select');
+            $(this).siblings().removeClass('select');
+        });
+
     };
     storeMapData();
 
@@ -101,36 +125,11 @@ $(function () {
             };
         });
     };
-    storeLink();
-
+    storeLink(); //리사이즈 필요함
 
     $(window).resize(function () {
         storeLink();
     });
-
-    let imageSrc = 'https://choibohui.github.io/hera/img/icon/map_marker.svg'; // 마커이미지의 주소입니다    
-    let imageSize = new kakao.maps.Size(40, 50); // 마커이미지의 크기입니다
-    let imageOption = { offset: new kakao.maps.Point(22, 50) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-    let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-    let markerPosition = new kakao.maps.LatLng(37.52611, 126.92862); // 초기 마커의 표시될 위치입니다
-
-    // 마커를 생성합니다
-    let marker = new kakao.maps.Marker({
-        position: markerPosition,
-        image: markerImage // 마커이미지 설정 
-    });
-
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
-
-    // 매장 리스트 선택 활성화
-    $('.storeList li').click(function () {
-        $(this).addClass('select');
-        $(this).siblings().removeClass('select');
-    });
-
 
 
 });
