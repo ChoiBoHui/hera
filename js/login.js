716183d63c8351458a35914043650063d65ab5c4
choibohui <123dfga@naver.com>
2023-06-25 00:04:29 +0900

    login update

--- js/login.js
+++ js/login.js
@@ -30,11 +30,36 @@ $(function () {
 
 });
 
+
+let count = 0;
 function loginCheck() {
+    let joinPlease = $('.joinPlease');
+    let findPW = $('.findPW');
+
+    //변수 증가
+    count = count + 1;
+    console.log(count);
+    if (count == 1) {
+        findPW.addClass("O-O")
+        joinPlease.addClass("clickPlease")
+    } else if (count == 2) {
+        joinPlease.addClass("hey")
+
+        let delay = 1000
+        joinPlease.addClass("vibration");
+        setTimeout(function () {
+            joinPlease.removeClass("vibration");
+        }, delay)
+
+    } else if (count == 3) {
+        joinPlease.addClass("fxxkingLogin")
+    } else if (count == 4) {
+        joinPlease.addClass("fxxkYou")
+    }
 
     alert("앗! 아직 회원이 아니시군요? 헤라의 회원이 되어주시겠어요?");
-    // 로그인 버튼 한번 누를때마다 회원가입 버튼 굵고 커지게 하고싶다. 귀엽고 재밌게!!
+    // 로그인 버튼 한번 누를때마다 회원가입 버튼 굵고 커지게 하고n싶다. 귀엽고 재밌게!!
 
     // 이후 액션은 개발자 부분
     return false;
-}
+};
