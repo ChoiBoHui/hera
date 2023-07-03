ad58e92ae400f7ddb7f95588782db67e100acf8d
choibohui <123dfga@naver.com>
2023-07-03 17:57:08 +0900

    join update

--- js/join.js
+++ js/join.js
@@ -27,211 +27,220 @@ $(function () {
     inputFocusEvent();
 
 
-    // Section id 아이디
-    var error = $('.id_area .error_next_box');
-    var condition1 = $('.idCondition .condition1');
-    var condition2 = $('.idCondition .condition2');
-    var id = $('#id');
-    let checkIcon1 = $('.idCondition .condition1 span')
-    let checkIcon2 = $('.idCondition .condition2 span')
+    function sectionId() {
+        // Section id 아이디
+        let error = $('.id_area .error_next_box');
+        let condition1 = $('.idCondition .condition1');
+        let condition2 = $('.idCondition .condition2');
+        let id = $('#id');
+        let checkIcon1 = $('.idCondition .condition1 span');
+        let checkIcon2 = $('.idCondition .condition2 span');
 
-    id.on("input", function () {
-        checkId();
-        checkLength();
-        checkCharacter();
-    });
+        id.on("input", function () {
+            checkId();
+            checkLength();
+            checkCharacter();
+        });
 
-    function checkId() {
-        if (id.val() === "") {
-            error.eq(0).html("필수 정보입니다.");
-            error.eq(0).css("display", "block");
-        } else {
-            error.eq(0).css("display", "none");
-        }
+        function checkId() {
+            if (id.val() === "") {
+                error.html("! 필수 정보입니다.");
+                error.css("display", "block");
+            } else {
+                error.css("display", "none");
+            }
+        };
+
+        function checkLength() {
+            let lengthPattern = /^.{5,20}$/;
+            if (!lengthPattern.test(id.val())) {
+                condition1.css("color", "#d30005");
+                checkIcon1.addClass('red').removeClass('green');
+            } else {
+                condition1.css("color", "#08A600");
+                checkIcon1.addClass('green').removeClass('red');
+            }
+        };
+
+        function checkCharacter() {
+            let characterPattern = /^[a-zA-Z0-9_-]*$/;
+            if (!characterPattern.test(id.val())) {
+                condition2.css("color", "#d30005");
+                checkIcon2.addClass('red').removeClass('green');
+            } else {
+                condition2.css("color", "#08A600");
+                checkIcon2.addClass('green').removeClass('red');
+            }
+        };
     };
+    sectionId();
 
-    function checkLength() {
-        var lengthPattern = /^.{5,20}$/;
-        if (!lengthPattern.test(id.val())) {
-            condition1.eq(0).css("color", "#d30005");
-            checkIcon1.addClass('red').removeClass('green');
-        } else {
-            condition1.eq(0).css("color", "#08A600");
-            checkIcon1.addClass('green').removeClass('red');
+
+    function sectionPw() {
+        // 비밀번호 pw
+        let pw1 = $('#pswd1');
+        let error = $('.pswd1_area .error_next_box');
+        let condition1 = $('.pwCondition .condition1');
+        let condition2 = $('.pwCondition .condition2');
+        let checkIcon1 = $('.pwCondition .condition1 span');
+        let checkIcon2 = $('.pwCondition .condition2 span');
+
+
+        pw1.on("input", function () {
+            checkPw();
+            checkLength();
+            checkCharacter();
+            checkPw2();
+        });
+
+        function checkPw() {
+            if (pw1.val() === "") {
+                error.html("! 필수 정보입니다.");
+                error.css("display", "block");
+            } else {
+                error.css("display", "none");
+            }
+        };
+
+        function checkLength() {
+            let lengthPattern = /^.{8,}$/;
+            if (!lengthPattern.test(pw1.val())) {
+                condition1.css("color", "#d30005");
+                checkIcon1.addClass('red').removeClass('green');
+            } else {
+                condition1.css("color", "#08A600");
+                checkIcon1.addClass('green').removeClass('red');
+            }
+        };
+
+        function checkCharacter() {
+            // const characterPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()]).{8,}$/;
+            const characterPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()])/
+
+            if (!characterPattern.test(pw1.val())) {
+                condition2.css("color", "#d30005");
+                checkIcon2.addClass('red').removeClass('green');
+            } else {
+                condition2.css("color", "#08A600");
+                checkIcon2.addClass('green').removeClass('red');
+            }
         }
+
+        // 비밀번호 pw
+        let pw2 = $('#pswd2');
+        let error2 = $('.pswd2_area .error_next_box');
+
+        pw2.on("input", function () {
+            checkPw2();
+        });
+
+        function checkPw2() {
+            if (pw2.val() !== pw1.val()) {
+                error2.html("! 비밀번호가 일치하지 않습니다.").css({
+                    color: "#d30005",
+                    display: "block"
+                });
+            } else {
+                error2.css("display", "none");
+            }
+        };
+
     };
+    sectionPw();
 
-    function checkCharacter() {
-        var characterPattern = /^[a-zA-Z0-9_-]*$/;
-        if (!characterPattern.test(id.val())) {
-            condition2.eq(0).css("color", "#d30005");
-            checkIcon2.addClass('red').removeClass('green');
-        } else {
-            condition2.eq(0).css("color", "#08A600");
-            checkIcon2.addClass('green').removeClass('red');
-        }
+    function joinDatepicker() {
+        $('.datepicker').datepicker();
+        $.datepicker.setDefaults({
+            dateFormat: 'yy-mm-dd',
+            // maxDate: new Date('2017-12-31'),
+            prevText: '이전 달',
+            nextText: '다음 달',
+            monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+            monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+            dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+            dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+            dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+            showOtherMonths: true, //빈 공간에 현재월의 앞뒤월의 날짜를 표시
+            showMonthAfterYear: true, // 월- 년 순서가아닌 년도 - 월 순서
+            changeYear: true, //option값 년 선택 가능
+            changeMonth: true, //option값  월 선택 가능
+            showOn: "both", //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시        
+            buttonImage:
+                "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif", //버튼 이미지 경로
+            buttonImageOnly: true, //버튼 이미지만 깔끔하게 보이게함
+            buttonText: "선택", //버튼 호버 텍스트
+            yearSuffix: '년',
+            // minDate: "-5Y", //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
+            minDate: new Date('1996-12-01'),
+            maxDate: "+5y", //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
+        });
+        //초기값을 오늘 날짜로 설정해줘야 합니다.
+        $(".datepicker").datepicker("setDate", "today"); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
     };
-
-
-
-
-    // pw
-    // 변수 선언
-    // var pw1 = document.querySelector('#pswd1');
-    // var pwMsg = document.querySelector('.lbl .step_txt');
-    // var pwImg1 = document.querySelector('.ps_box.int_pass:after');
-
-
-    // pw1.addEventListener("change", checkPw);
-
-    // function checkPw() {
-    //     var pwPattern = /^[a-zA-Z0-9~!@#$%^&*()_+|<>?:{}]{8,16}$/;
-    //     if (pw1.value === "") {
-    //         error[1].innerHTML = "필수 정보입니다.";
-    //         pwMsg.style.display = "block";
-    //         pwMsgArea.style.paddingRight = "40px";
-    //         pwImg1.src = "m_icon_pass.png";
-    //         error[1].style.display = "block";
-    //     } else if (!pwPattern.test(pw1.value)) {
-    //         error[1].innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
-    //         pwMsg.innerHTML = "사용불가";
-    //         pwMsgArea.style.paddingRight = "93px";
-    //         error[1].style.display = "block";
-    //         pwMsg.style.color = "red";
-    //         pwMsg.style.display = "block";
-    //         pwImg1.src = "m_icon_not_use.png";
-    //     } else {
-    //         error[1].style.display = "none";
-    //         pwMsg.innerHTML = "안전";
-    //         pwMsgArea.style.paddingRight = "93px";
-    //         pwMsg.style.color = "#03c75a";
-    //         pwMsg.style.display = "block";
-    //         pwImg1.src = "m_icon_safe.png";
-    //     }
-    // }
-
-
-
-
-
-    // 아이디 id 부분
-    // function checkId(event) {
-    //     if (idFlag) return true;
-
-    //     var id = $("#id").val();
-    //     var oMsg = $("#idMsg");
-    //     var oInput = $("#id");
-
-    //     if (id === "") {
-    //         showErrorMsg(oMsg, "필수 정보입니다.");
-    //         setFocusToInputObject(oInput);
-    //         return false;
-    //     }
-
-    //     var isID = /^[a-z0-9][a-z0-9_\-]{4,19}$/;
-    //     if (!isID.test(id)) {
-    //         showErrorMsg(oMsg, "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.");
-    //         setFocusToInputObject(oInput);
-    //         return false;
-    //     }
-
-    //     idFlag = false;
-    //     var key = $("#token_sjoin").val();
-
-    //     $.ajax({
-    //         type: "GET",
-    //         url: "/user2/joinAjax?m=checkId&id=" + id + "&key=" + key,
-    //         success: function (data) {
-    //             var result = data.substr(4);
-
-    //             if (result == "Y") {
-    //                 if (event == "first") {
-    //                     showSuccessMsg(oMsg, "멋진 아이디네요!");
-    //                 } else {
-    //                     hideMsg(oMsg);
-    //                 }
-    //                 idFlag = true;
-    //             } else {
-    //                 showErrorMsg(oMsg, "이미 사용중이거나 탈퇴한 아이디입니다.");
-    //                 setFocusToInputObject(oInput);
-    //             }
-    //         }
-    //     });
-    //     return true;
-    // }
-
-    // 뭐 했을떄 실패는 에러, 성공은 그린 붙여서 나타내란건 알겠다요
-    // function showErrorMsg(obj, msg) {
-    //     obj.attr("class", "error_next_box");
-    //     obj.html(msg);
-    //     obj.show();
-    // }
-    // function showSuccessMsg(obj, msg) {
-    //     obj.attr("class", "error_next_box green");
-    //     obj.html(msg);
-    //     obj.show();
-    // }
-
-
+    joinDatepicker();
 
     //주민번호 확인 함수
-    // function check_jumin() {
-    //     var jumins3 = $("#pnum").val() + $("#nnum").val();
-    //     //주민등록번호 생년월일 전달
+    function check_jumin() {
+        let jumins3 = $("#pnum").val() + $("#nnum").val();
+        //주민등록번호 생년월일 전달
 
-    //     var fmt = RegExp(/^\d{6}[1234]\d{6}$/)  //포멧 설정
-    //     var buf = new Array(13);
+        let fmt = RegExp(/^\d{6}[1234]\d{6}$/)  //포멧 설정
+        let buf = new Array(13);
 
-    //     //주민번호 유효성 검사
-    //     if (!fmt.test(jumins3)) {
-    //         alert("주민등록번호 형식에 맞게 입력해주세요");
-    //         $("#pnum").focus();
-    //         return false;
-    //     }
+        //주민번호 유효성 검사
+        if (!fmt.test(jumins3)) {
+            alert("주민등록번호 형식에 맞게 입력해주세요");
+            $("#pnum").focus();
+            return false;
+        }
 
-    //     //주민번호 존재 검사
-    //     for (var i = 0; i < buf.length; i++) {
-    //         buf[i] = parseInt(jumins3.charAt(i));
-    //     }
+        //주민번호 존재 검사
+        for (let i = 0; i < buf.length; i++) {
+            buf[i] = parseInt(jumins3.charAt(i));
+        }
 
-    //     var multipliers = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5];// 밑에 더해주는 12자리 숫자들 
-    //     var sum = 0;
+        let multipliers = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5];// 밑에 더해주는 12자리 숫자들 
+        let sum = 0;
 
-    //     for (var i = 0; i < 12; i++) {
-    //         sum += (buf[i] *= multipliers[i]);// 배열끼리12번 돌면서 
-    //     }
+        for (let i = 0; i < 12; i++) {
+            sum += (buf[i] *= multipliers[i]);// 배열끼리12번 돌면서 
+        }
 
-    //     if ((11 - (sum % 11)) % 10 != buf[12]) {
-    //         alert("잘못된 주민등록번호 입니다.");
-    //         $("#pnum").focus();
-    //         return false;
-    //     }
+        if ((11 - (sum % 11)) % 10 != buf[12]) {
+            alert("잘못된 주민등록번호 입니다.");
+            $("#pnum").focus();
+            return false;
+        }
 
-    //     var birthYear = (jumins3.charAt(6) <= "2") ? "19" : "20";
-    //     birthYear += $("#pnum").val().substr(0, 2);
-    //     var birthMonth = $("#pnum").val().substr(2, 2);
-    //     var birthDate = $("#pnum").val().substr(4, 2);
-    //     var birth = new Date(birthYear, birthMonth, birthDate);
+        let birthYear = (jumins3.charAt(6) <= "2") ? "19" : "20";
+        birthYear += $("#pnum").val().substr(0, 2);
+        let birthMonth = $("#pnum").val().substr(2, 2);
+        let birthDate = $("#pnum").val().substr(4, 2);
+        let birth = new Date(birthYear, birthMonth, birthDate);
 
 
-    //     $("#year").val(birthYear);
-    //     $("#month").val(birthMonth);
-    //     $("#day").val(birthDate);
+        $("#year").val(birthYear);
+        $("#month").val(birthMonth);
+        $("#day").val(birthDate);
+    };
+    // check_jumin();
 
-    // }
+
+
+
 });
 
 
 
 // 일단 긁어온거라서 아직 뭔지 확인 안했음
 // function check() {
-//     var getIntro = $("#intro").val().replace(/\s|/gi, '');
-//     var hobbyCheck = false;
-//     var getMail = RegExp(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/);
-//     var getCheck = RegExp(/^[a-zA-Z0-9]{4,12}$/);
-//     var getName = RegExp(/^[가-힣]+$/);
-//     var fmt = RegExp(/^\d{6}[1234]\d{6}$/); //형식 설정
-//     var buf = new Array(13); //주민등록번호 배열
+//     let getIntro = $("#intro").val().replace(/\s|/gi, '');
+//     let hobbyCheck = false;
+//     let getMail = RegExp(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/);
+//     let getCheck = RegExp(/^[a-zA-Z0-9]{4,12}$/);
+//     let getName = RegExp(/^[가-힣]+$/);
+//     let fmt = RegExp(/^\d{6}[1234]\d{6}$/); //형식 설정
+//     let buf = new Array(13); //주민등록번호 배열
 
 
 //     //아이디 공백 확인
@@ -308,7 +317,7 @@ $(function () {
 //     }
 
 //     //관심분야
-//     for (var i = 0; i < $('[name="hobby[]"]').length; i++) {
+//     for (let i = 0; i < $('[name="hobby[]"]').length; i++) {
 //         if ($('input:checkbox[name="hobby[]"]').eq(i).is(":checked") == true) {
 //             hobbyCheck = true;
 //             break;
